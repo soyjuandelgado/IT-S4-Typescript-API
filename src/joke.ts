3ed3d30880c9ff9e03fe77f50d760910e233f0e4
Juan Delgado <soy.juan.delgado@gmail.com>
2025-06-12 11:10:14 +0200

    exercise 3 base

--- src/joke.ts
+++ src/joke.ts
@@ -1,12 +1,51 @@
-export const getJoke = () : Promise<any> => {
+interface Joke {
+    joke: string;
+    score: number;
+    date: string; //ISO Date
+}
+
+let reportJokes: Joke[] = [];
+
+/* MODIFICAR:
+    - Devolver solo el texto de la broma en getJoke
+    - Gestion de errores en GetJokeAPI
+    - Solo añadir en reportJokes cuando se valora
+    - Buscar si existe en reportJokes
+    - ¿Utilizar clases con metodos?: clase joke que se extendera por api
+ */
+
+export const getJoke = (): Promise<any> => {
+    return getJokeAPI().then((response) => {
+        if (response.status == 200) {
+            reportJokes.push({
+                joke: response.joke,
+                score: 0,
+                date: new Date().toISOString(),
+            });
+        }else{
+            throw Error(response.statusText)
+        }
+        return response;
+    });
+};
+
+const getJokeAPI = (): Promise<any> => {
     const conf = {
         method: "GET",
         headers: {
             Accept: "application/json",
-            "User-Agent": "IT API (https://github.com/soyjuandelgado/IT-S4-Typescript-API)"
-        }
-    }
+            "User-Agent":
+                "IT API (https://github.com/soyjuandelgado/IT-S4-Typescript-API)",
+        },
+    };
     return fetch("https://icanhazdadjoke.com/", conf)
-        .then(res => res.ok ? res.json() : res)
-        .then(response => response);
-}
+        .then((res) => (res.ok ? res.json() : res))
+        .then((response) => response);
+};
+
+export const scoreJoke = (value: number) => {
+    let lastJoke = reportJokes[reportJokes.length - 1];
+    lastJoke.score = value;
+    lastJoke.date = new Date().toISOString();
+    console.log(reportJokes);
+};
--- src/main.ts
+++ src/main.ts
@@ -1,19 +1,32 @@
-import { getJoke } from "./joke";
+import { getJoke, scoreJoke } from "./joke";
 
-document.addEventListener("DOMContentLoaded", () =>{
-    const button = document.getElementById("next");
-    if (button)
-        button.addEventListener("click", nextJoke);
-    
+document.addEventListener("DOMContentLoaded", () => {
+    const btnNext = document.getElementById("next");
+    if (btnNext) btnNext.addEventListener("click", nextJoke);
+
+    const btnValJoke1 = document.getElementById("joke-val-1");
+    const btnValJoke2 = document.getElementById("joke-val-2");
+    const btnValJoke3 = document.getElementById("joke-val-3");
+    if (btnValJoke1)
+        btnValJoke1.addEventListener("click", valJoke.bind(null, 1));
+    if (btnValJoke2) btnValJoke2.addEventListener("click", () => valJoke(2));
+    if (btnValJoke3) btnValJoke3.addEventListener("click", () => valJoke(3));
+
     nextJoke();
 });
 
 const nextJoke = async () => {
     const joke = document.getElementById("joke-text");
-    if (joke){
+    if (joke) {
         const jokeObj = await getJoke();
         console.log(jokeObj);
-        joke.textContent = jokeObj.status == 200 ? jokeObj.joke : jokeObj.status;
+        joke.textContent =
+            jokeObj.status == 200 ? jokeObj.joke : jokeObj.status;
         //getJoke().then( text => joke.textContent = text.joke);
     }
-}
+};
+
+const valJoke = (value: number) => {
+    alert(value);
+    scoreJoke(value);
+};

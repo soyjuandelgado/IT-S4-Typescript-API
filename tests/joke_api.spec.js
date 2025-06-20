fdcc147ce931cefcda9d5af12b6e00a34b98b879
Juan Delgado <soy.juan.delgado@gmail.com>
2025-06-20 12:36:14 +0200

    feat: add joke_api tests

--- tests/joke_api.spec.js
+++
@@ -1,8 +0,0 @@
-import { getJoke, getJokeAPI } from "../src/joke_api";
-//const { getJoke, getJokeAPI } = require("../src/joke_api");
-
-describe('Function "getJoke"', () => {
-    it("should be declared", () => {
-        expect(typeof getJoke).toBe("function");
-    });
-});
